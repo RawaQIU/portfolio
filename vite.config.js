0a8631d565f384df6c8bd7bc429b1810ba5e4501
RawaQIU <rdqiu230196@uniq.edu.iq>
2024-03-27 13:26:17 +0300

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    // base: '/portfolio',
-    // resolve: {
-    //     alias: {
-    //         '@': Path.resolve(__dirname, './src'),
-    //     },
-    // },
+    base: '/portfolio/',
 });
